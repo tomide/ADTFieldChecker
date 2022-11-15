a29b95b1310d9d42a0ecd85e9400ee4cf25d1d1f
Tom Adedeji <tadedeji@tubi.tv>
2022-11-15 13:52:52 -0700

    - refactored to use basic scala comparison method

--- src/test/scala/AssertChecker.scala
+++ src/test/scala/AssertChecker.scala
@@ -1,6 +1,5 @@
 import com.stephenn.scalatest.circe.JsonMatchers
-import io.circe.{Decoder, Encoder}
-import io.circe.parser.decode
+import io.circe.Encoder
 import io.circe.syntax._
 import org.scalatest.Succeeded
 import org.scalatest.funsuite.AnyFunSuite
@@ -8,15 +7,23 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 trait AssertChecker extends AnyFunSuite with JsonMatchers  {
 
-
   def compare[A](left: A, right: A)(implicit jsonSerializer: Encoder[A]): Unit = {
+    if (left === right) {
+      ()
+    } else {
+      printDiff(left, right)
+    }
+  }
 
+  protected def printDiff[A](left: A, right: A)(implicit jsonSerializer: Encoder[A]): Unit = {
   val a = left.asJson.noSpaces
   val b = right.asJson.noSpaces
   a should matchJson(b) match {
     case Succeeded => ()
     case err => pprint.pprintln(err)
-    }
   }
+  }
+
+
 
 }
--- src/test/scala/TestSpec.scala
+++ src/test/scala/TestSpec.scala
@@ -1,17 +1,18 @@
 import io.circe.Encoder
 import io.circe.generic.extras.Configuration
 import io.circe.generic.semiauto.deriveEncoder
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class TestSpec extends AssertChecker  {
 
 
-  case class Person(name: String)
+  case class PersonAddress(streetNumber: Long, streetName: String)
+  case class Person(name: String, address: PersonAddress)
     implicit val codecConfiguration: Configuration =
       Configuration.default.withSnakeCaseMemberNames.withDefaults
     implicit val personEncoder: Encoder[Person] = deriveEncoder
+    implicit val personAddressEncoder: Encoder[PersonAddress] = deriveEncoder
 
-
-  compare(Person("tom"), Person("json"))
+  compare(Person("tom", PersonAddress(100L, "tubi")),
+    Person("tom", PersonAddress(10L, "tubi")))
 
 }
